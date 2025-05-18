d52ef7ace1bfd50673369ab907a5422ff648a6cf
NjabuloJ <fanajb65@gmail.com>
2025-05-18 17:01:52 +0200

    Create repo.js

---
+++ commandes/repo.js
@@ -0,0 +1,136 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { fana } = require("../njabulo/fana");
+
+fana({ nomCom: "sc", catégorie:"Général", reaction: "✨", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+  const githubRepo = 'https://api.github.com/repos/NjabuloJ/Njabulo-Jb';
+  const img = 'https://files.catbox.moe/0cxusf.jpg';
+
+  try {
+    const response = await fetch(githubRepo);
+    const data = await response.json();
+
+    if (data) {
+      const repoInfo = {
+        stars: data.stargazers_count,
+        forks: data.forks_count,
+        lastUpdate: data.updated_at,
+        owner: data.owner.login,
+      };
+
+      const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
+      const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
+
+      const gitdata = `*hellow Friend
+this is* *ᴅᴜʟʟᴀʜ-xᴍᴅ v² 👊.*\n *Join Group Chat* https://chat.whatsapp.com/IdRXU9UcO8K50GPelOyhxh
+
+🗼 *REPOSITORY:* ${data.html_url}
+💫 *STARS:* ${repoInfo.stars}
+🧧 *FORKS:* ${repoInfo.forks}
+📅 *RELEASE DATE:* ${releaseDate}
+🕐 *UPDATE ON:* ${repoInfo.lastUpdate}
+__________________________________
+            *Made With ᴅᴜʟʟᴀʜ-xᴍᴅ v²*`;
+
+  // Two sets of images to display randomly
+    const extraImages1 = [
+        "https://files.catbox.moe/nj1w1s.jpg",
+        "https://files.catbox.moe/znvqsv.jpg",
+        "https://files.catbox.moe/nj1w1s.jpg"
+    ];
+
+    const extraImages2 = [
+        "https://files.catbox.moe/znvqsv.jpg",
+        "https://files.catbox.moe/nj1w1s.jpg",
+        "https://files.catbox.moe/znvqsv.jpg"
+    ];
+
+    // Randomly select which menu to show
+    const isOriginalMenu = Math.random() > 0.5; // 50% chance for either menu
+
+    let mediaUrl, thumbnail, renderType;
+    if (isOriginalMenu) {
+        mediaUrl = mybotpic(); // Use bot’s original picture
+        thumbnail = extraImages1[Math.floor(Math.random() * extraImages1.length)];
+        renderType = "renderLargerThumbnail";
+    } else {
+        mediaUrl = extraImages2[Math.floor(Math.random() * extraImages2.length)];
+        thumbnail = mediaUrl; // Use the same image as media
+        renderType = "renderSmallThumbnail";
+    }
+
+    try {
+        if (mediaUrl.match(/\.(mp4|gif)$/i)) {
+            await zk.sendMessage(dest, {
+                video: { url: mediaUrl },
+                caption: gitdata,
+                footer: "*Njabulo Jb*, developed by Njabulo",
+                gifPlayback: true,
+                contextInfo: {
+                    externalAdReply: {
+                        title: "Njabulo Jb",
+                        body: "ɴᴊᴀʙᴜʟᴏ ᴊʙ ᴍᴜʟᴛɪ ᴅᴇᴠɪᴄᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ",
+                        mediaType: 1,
+                        thumbnailUrl: thumbnail,
+                        sourceUrl: "https://github.com/NjabuloJ/Njabulo-Jb",
+                        showAdAttribution: true,
+                        [renderType]: true, // Apply correct thumbnail size
+                    },
+                },
+            }, { quoted: ms });
+        } else {
+            await zk.sendMessage(dest, {
+                image: { url: mediaUrl },
+                caption: gitdata,
+                footer: "*Njabulo_Jb*, developed by Njabulo",
+                contextInfo: {
+                    externalAdReply: {
+                        title: "Njabulo Jb",
+                        body: "ɴᴊᴀʙᴜʟᴏ ᴊʙ ᴍᴜʟᴛɪ ᴅᴇᴠɪᴄᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ",
+                        mediaType: 1,
+                        thumbnailUrl: thumbnail,
+                        sourceUrl: "https://github.com/NjabuloJ/Njabulo-Jb",
+                        showAdAttribution: true,
+                        [renderType]: true, // Apply correct thumbnail size
+                    },
+                },
+            }, { quoted: ms });
+        }
+    } catch (e) {
+        console.log("🥵🥵 Error sending repo: " + e);
+        repondre("🥵🥵 Error sending repo: " + e);
+    }
+
+    // List of audio URLs
+    const audioUrls = [
+        "https://files.catbox.moe/6x0rb7.mp3" // New song added
+    ];
+
+    // Select a random audio file
+    const randomAudioUrl = audioUrls[Math.floor(Math.random() * audioUrls.length)];
+
+    try {
+        await zk.sendMessage(dest, {
+            audio: { url: randomAudioUrl },
+            mimetype: 'audio/mpeg',
+            ptt: true, // Send as a voice note
+             contextInfo: {
+               externalAdReply: {
+               title: "song menu",
+               body: "ɴᴊᴀʙᴜʟᴏ ᴊʙ ᴍᴜʟᴛɪ ᴅᴇᴠɪᴄᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ",
+               mediaType: 1,
+               thumbnailUrl: thumbnail,
+               sourceUrl: "https://github.com/NjabuloJ/Njabulo-Jb",
+               showAdAttribution: true,
+              [renderType]: true, // Apply correct thumbnail size
+              },
+            },
+        }, { quoted: ms });
+    } catch (e) {
+        console.log("🥵🥵 Error sending audio: " + e);
+        repondre("🥵🥵 Error sending audio: " + e);
+    }
+});
+
+
+      
