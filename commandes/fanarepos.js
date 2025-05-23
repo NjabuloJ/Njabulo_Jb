2e4415b96d949cc4dd1bb9d971c591eaaef17762
NjabuloJ <fanajb65@gmail.com>
2025-05-23 12:02:54 +0200

    Create fanarepos.js

---
+++ commandes/fanarepos.js
@@ -0,0 +1,156 @@
+const util = require('util');
+const fs = require('fs-extra');
+const { fana } = require(__dirname + "/../njabulo/fana");
+const { format } = require(__dirname + "/../njabulo/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+
+fana({ nomCom: "repo", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre, prefixe, nomAuteurMessage, mybotpic } = commandeOptions;
+    let { cm } = require(__dirname + "/../njabulo//fana");
+    var coms = {};
+    var mode = s.MODE.toLowerCase() !== "yes" ? "private" : "public";
+
+    cm.map(async (com) => {
+        if (!coms[com.categorie]) coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault("Africa/Nairobi");
+    const temps = moment().format('HH:mm:ss');
+    const date = moment().format('DD/MM/YYYY');
+
+    // Generate greeting based on time of day
+    const hour = moment().hour();
+    let greeting = "Good Morning";
+    if (hour >= 12 && hour < 18) {
+        greeting = "Good afternnon!";
+    } else if (hour >= 18) {
+        greeting = "Good Everning!";
+    } else if (hour >= 22 || hour < 5) {
+        greeting = "Good Night 🌌";
+    }
+
+    let infoMsg = `
+   
+            *This is Alec-Jb*
+            the best bot in the universe developed by Njabulo Jb. Fork and give a star 🌟 to my repo!
+            
+> Njabulo JB🟢 
+> https://github.com/NjabuloJ/Njabulo-Jb
+
+> Alec Jb🟢
+> https://github.com/NjabuloJ/Alec_Jb
+[🌟1k] [🍴2k]
+⚙️ *About the Bot:*
+This bot is built by Njabulo JB experts to enhance WhatsApp experience. Packed with 100+ commands, including:
+• Logo & banner makers
+• AI chatbot
+• Downloader tools
+• Group manager & more!
+• oll commandes work 
+• no problem on this bot 
+•: ${greeting}
+
+⚠️ Use responsibly. We’re not liable for any damage or bans.`;
+
+    // Two sets of images to display randomly
+    const extraImages1 = [
+        "https://files.catbox.moe/nj1w1s.jpg",
+        "https://files.catbox.moe/znvqsv.jpg",
+        "https://files.catbox.moe/nj1w1s.jpg"
+    ];
+
+    const extraImages2 = [
+        "https://files.catbox.moe/znvqsv.jpg",
+        "https://files.catbox.moe/nj1w1s.jpg",
+        "https://files.catbox.moe/znvqsv.jpg"
+    ];
+
+    // Randomly select which menu to show
+    const isOriginalMenu = Math.random() > 0.5; // 50% chance for either menu
+
+    let mediaUrl, thumbnail, renderType;
+    if (isOriginalMenu) {
+        mediaUrl = mybotpic(); // Use bot’s original picture
+        thumbnail = extraImages1[Math.floor(Math.random() * extraImages1.length)];
+        renderType = "renderLargerThumbnail";
+    } else {
+        mediaUrl = extraImages2[Math.floor(Math.random() * extraImages2.length)];
+        thumbnail = mediaUrl; // Use the same image as media
+        renderType = "renderSmallThumbnail";
+    }
+
+    try {
+        if (mediaUrl.match(/\.(mp4|gif)$/i)) {
+            await zk.sendMessage(dest, {
+                video: { url: mediaUrl },
+                caption: infoMsg,
+                footer: "*Njabulo Jb*, developed by Njabulo",
+                gifPlayback: true,
+                contextInfo: {
+                    externalAdReply: {
+                        title: "Alec-Jb",
+                        body: "ᴍᴜʟᴛɪ ᴅᴇᴠɪᴄᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ",
+                        mediaType: 1,
+                        thumbnailUrl: thumbnail,
+                        sourceUrl: "https://github.com/NjabuloJ/Njabulo-Jb",
+                        showAdAttribution: true,
+                        [renderType]: true, // Apply correct thumbnail size
+                    },
+                },
+            }, { quoted: ms });
+        } else {
+            await zk.sendMessage(dest, {
+                image: { url: mediaUrl },
+                caption: infoMsg,
+                footer: "*Njabulo_Jb*, developed by Njabulo",
+                contextInfo: {
+                    externalAdReply: {
+                        title: "Alec-Jb",
+                        body: "ᴍᴜʟᴛɪ ᴅᴇᴠɪᴄᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ",
+                        mediaType: 1,
+                        thumbnailUrl: thumbnail,
+                        sourceUrl: "https://github.com/NjabuloJ/Njabulo-Jb",
+                        showAdAttribution: true,
+                        [renderType]: true, // Apply correct thumbnail size
+                    },
+                },
+            }, { quoted: ms });
+        }
+    } catch (e) {
+        console.log("🥵🥵 Error sending menu: " + e);
+        repondre("🥵🥵 Error sending menu: " + e);
+    }
+
+    // List of audio URLs
+    const audioUrls = [
+        "https://files.catbox.moe/6x0rb7.mp3" // New song added
+    ];
+
+    // Select a random audio file
+    const randomAudioUrl = audioUrls[Math.floor(Math.random() * audioUrls.length)];
+
+    try {
+        await zk.sendMessage(dest, {
+            audio: { url: randomAudioUrl },
+            mimetype: 'audio/mpeg',
+            ptt: true, // Send as a voice note
+             contextInfo: {
+               externalAdReply: {
+               title: "Alec-Jb",
+               body: "ᴍᴜʟᴛɪ ᴅᴇᴠɪᴄᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ",
+               mediaType: 1,
+               thumbnailUrl: thumbnail,
+               sourceUrl: "https://github.com/NjabuloJ/Njabulo-Jb",
+               showAdAttribution: true,
+              [renderType]: true, // Apply correct thumbnail size
+              },
+            },
+        }, { quoted: ms });
+    } catch (e) {
+        console.log("🥵🥵 Error sending audio: " + e);
+        repondre("🥵🥵 Error sending audio: " + e);
+    }
+});
