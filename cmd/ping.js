4a6f438e90b1252d80e6cf6a745818178bd9dce2
NjabuloJ <fanajb65@gmail.com>
2025-08-01 17:33:59 +0200

    Create ping.js

---
+++ cmd/ping.js
@@ -0,0 +1,44 @@
+const {
+  zokou
+} = require("./../framework/zokou");
+const {
+  format,
+  runtime
+} = require('../njabulo/mesfonctions');
+const os = require('os');
+const speed = require('performance-now');
+const {
+  performance
+} = require('perf_hooks');
+const conf = require('../set');
+
+zokou(
+  {
+    nomCom: 'ping',
+    categorie: 'General',
+    reaction: '📟',
+    alias: ['p']
+  },
+
+  async (dest, zk, commandOptions) => {
+    const {
+      ms, arg, repondre
+    } = commandOptions;
+    const start = new Date().getTime();
+    const msg = await zk.sendMessage(dest, {
+      text: '⏰',
+    }, {
+      quoted: ms
+    });
+    const end = new Date().getTime();
+    const ping = end - start;
+    await zk.sendMessage(dest, {
+      text: `Pong▰▰▰▱▱▱ ${ping} ms`, edit: {
+        id: msg.key.id, remoteJid: dest
+      }});
+    await zk.sendMessage(dest, {
+      react: {
+        text: "🏓", key: ms.key
+      }})
+  }
+)
